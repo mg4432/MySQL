82c620d6af27474c0157e66b3510c4d6a898718b
mg4432 <mg4432@pusan.ac.kr>
2022-02-10 17:57:36 +0900

    study

--- Day2.sql
+++ Day2.sql
@@ -1,6 +1,67 @@
-# MISSION : 포켓몬 데이터베이스와 나의 포켓몬 테이블을 만들고, 캐터피, 피카츄, 이브이의 포켓몬 번호, 영문 이름, 타입 데이터 삽임 
+/*
+# CREATE TABLE : 테이블 만들기 
+# CREATE TABLE [테이블 이름] (
+		[컬럼 이름] [데이터 타입],
+		[컬럼 이름] [데이터 타입],
+		...
+		) ;
+*/
 
 /*
+# ALTER TABLE : 테이블 변경
+# ALTER TABLE [테이블 이름] RENAME [새로운 테이블 이름] ;  테이블 이름 변경
+# ex. ALTER TABLE costomer RENAME customers ; 
+
+# ALTER TABLE [테이블 이름] ADD COLUMN [컬럼 이름] [데이터 타입] ;  새로운 컬럼 추가
+# ex. ALTER TABLE customers ADD COLUMN age INT; 
+
+# ALTER TABLE [테이블 이름] MODIFY [컬럼 이름] [새로운 데이터 타입] ;  컬럼 타입 변경
+# ex. ALTER TABLE customers ADD COLUMN age FLOAT; 
+
+## 컬럼 이름과 데이터 타입 모두 바꾸는 경우는 컬럼 전체를 바꾸는 것이기 때문에 CHANGE COLUMN
+# ALTER TABLE [테이블 이름] 
+  CHANGE COLUMN [컬럼 이름] [새로운 컬럼 이름] [새로운 데이터 타입] ;  컬럼 이름과 타입 변경
+# ex. ALTER TABLE customers 
+	  CHANGE COLUMN age new_age FLOAT;  컬럼 이름과 타입 변경
+      
+# ALTER TABLE [테이블 이름] DROP COLUMN [컬럼 이름] ;  컬럼 삭제
+# ex. ALTER TABLE customers DROP COLUMN new_age ;
+*/
+
+/*
+# TRUNCATE TABLE [테이블 이름] : 해당 테이블의 값 삭제(테이블 자체는 남아있음)
+*/
+
+/*
+# DROP DATABASE [데이터 베이스 이름] : 데이터베이스 삭제
+# DROP DATABASE IF EXISTS [데이터 베이스 이름] : 데이터베이스 삭제 (존재한다면)
+# DROP TABLE IF EXISTS [데이터 베이스 이름] : 데이터베이스 삭제 (존재한다면)
+*/
+
+/*
+# INSERT INTO [테이블 이름] ([컬럼1 이름], [컬럼2 이름], ...)
+  VALUES([컬럼1 값], [컬럼2 값], ...) : 데이터 삽입하기
+# ex. INSERT INTO idol (name, age, group)
+	  VALUES ('제니', 27, '블랙핑크') ;
+
+# DELETE FROM [테이블 이름]	
+  WHERE [조건 값] : 데이터 삭제하기
+
+# UPDATE [테이블 이름]
+  SET [컬럼 이름] = [새 값]
+  WHERE [조건 값] ; : 데이터 수정하기
+*/
+
+
+/*
+# DROP DATABASE [데이터 베이스 이름] : 데이터베이스 삭제
+# DROP DATABASE IF EXISTS [데이터 베이스 이름] : 데이터베이스 삭제 (존재한다면)
+# DROP TABLE IF EXISTS [데이터 베이스 이름] : 데이터베이스 삭제 (존재한다면)
+*/
+
+
+# PRACTICE 1
+/* MISSION 1
 STEP 1. 포켓몬 데이터베이스 생성(이름 : pokemon)
 STEP 2. 포켓몬 데이터베이스 안에 나의 포켓몬 테이블 생성(테이블 이름 : mypokemon)
 	    이 때, Column은 (1) 포켓몬 번호, (2) 포켓몬 영문 이름, (3) 포켓몬 타입 총 3가지로 하고 이름과 데이터 타입 지정 
@@ -10,6 +71,7 @@ STEP 3. 포켓몬 번호 영문이름    타입
         피카츄 25  pikachu   electric
         이브이 133 eevee     normal
 */
+
 
 CREATE DATABASE pokemon ; 
 
@@ -30,3 +92,61 @@ VALUES (10, 'caterpie', 'bug'),
        
 SELECT *
 FROM mypokemon;
+
+/* MISSION 2
+MISSION 1에서 만든 포켓몬 데이터베이스에 '나의 새로운 포켓몬(mynewpokemon) 테이블을 만들고 포니타, 메타몽, 뮤의 포켓몬 번호, 이름, 타입 데이터 삽입
+STEP 1. 포켓몬 데이터베이스 안에 새로운 포켓몬 테이블 생성(mynewpokemon)
+		컬럼은 (1) 포켓몬 번호, (2) 포켓몬 영문 이름, (3) 포켓몬 타입 3가지 (number : INT, name : VARCHAR(20), type : VARCHAR(10))
+	
+STEP 2. mynewpokemon 테이블 안에 포켓몬 데이터를 각각의 로우로 삽입
+STEP 3. 포켓몬 번호 영문이름    타입
+		캐퍼티 10  caterpie  bug
+        피카츄 25  pikachu   electric
+        이브이 133 eevee     normal
+*/
+
+# STEP 1 
+USE pokemon ;
+
+CREATE TABLE mynewpokemon (
+	number INT,
+    name VARCHAR(20),
+    type VARCHAR(10)
+    ) ;
+    
+# STEP 2 
+INSERT INTO mynewpokemon (number, name, type)
+VALUES (77, '포니타', '불꽃'),
+	   (132, '메타몽', '노말'),
+       (151, '뮤', '에스퍼') ;
+SELECT *
+FROM mynewpokemon;
+
+# PRACTICE 2
+/* MISSION 1 
+포켓몬 데이터베이스 안에 있는 mypokemon테이블과 mynewpokemon 테이블 변경 
+
+STEP 1. mypokemon 테이블의 이름을 myoldpokemon으로 변경
+STEP 2. myoldpokemon 테이블의 'name'컬럼의 이름을 'eng_nm'으로 변경 (VARCHAR(20))
+STEP 3. mynewpokemon 테이블의 'name'컬럼의 이름을 'kor_nm'으로 변경 (VARCHAR(20))
+*/
+
+# STEP 1. mypokemon 테이블의 이름을 myoldpokemon으로 변경
+ALTER TABLE mypokemon
+RENAME myoldpokemon ; 
+
+# STEP 2. myoldpokemon 테이블의 'name'컬럼의 이름을 'eng_nm'으로 변경 (VARCHAR(20))
+ALTER TABLE myoldpokemon
+CHANGE COLUMN name eng_nm VARCHAR(20) ; 
+
+# STEP 3. mynewpokemon 테이블의 'name'컬럼의 이름을 'kor_nm'으로 변경 (VARCHAR(20))
+ALTER TABLE mynewpokemon
+CHANGE COLUMN name kor_nm VARCHAR(20) ; 
+
+/* MISSION 2
+포켓몬 데이터베이스 안에 있는 myoldpokemon 테이블은 값만 지우고 mynewpokemon 테이블은 전부 삭제
+*/
+
+TRUNCATE TABLE myoldpokemon ; 
+DROP TABLE mynewpokemon;
+
--- Day3.sql
+++ Day3.sql
@@ -39,52 +39,88 @@ FROM [테이블 이름];
 */
 
 
-/* DAY3 실습
-# MISSION 1. 포켓몬 테이블에서 포켓몬들의 이름과 키, 몸무게 가져오기
-# MISSION 2. 포켓몬 테이블에서 포켓몬들의 이름과 공격력, 방어력, 속도 가져오기 
-# MISSION 3. 포켓몬 테이블에서 포켓몬들의 이름과 능력치의 합 가져오기 
-	- Condition1. 능력치의 합은 공격력, 방어력, 속도의 합을 의미
-    - Condition2. 능력치의 합은 'total'이라는 별명으로 가져오기
-    - HINT. 숫자형 데이터 타입 컬럼 간에는 연산이 가능 
-*/
-
-# 포켓몬 데이터 입력 
-DROP DATABASE IF EXISTS pokemon; # pokemon 데이터베이스 삭제
-CREATE DATABASE pokemon ; 
+# PRACTICE
+DROP DATABASE IF EXISTS pokemon ; 
+CREATE DATABASE pokemon ;
 USE pokemon ; 
-CREATE TABLE mypokemon(
-		number INT, 
-        name VARCHAR(20),
-        type VARCHAR(20),
-        height FLOAT,
-        weight FLOAT,
-        attack FLOAT,
-        defense FLOAT,
-        speed FLOAT
-        ) ;
-INSERT INTO mypokemon (number, name, type, height, weight, attack, defense, speed)
-VALUES (10, 'caterpie', 'bug', 0.3, 2.9, 30, 35, 45),
-(25, 'pikachu', 'electric', 0.4, 6, 55, 40, 90),
-(26, 'raichu', 'electric', 0.8, 30, 90, 55, 110),
-(133, 'eevee', 'normal', 0.3, 6.5, 55, 50, 55),
-(152, 'chikoirita', 'grass', 0.9, 6.4, 49, 65, 45);
 
-SELECT * 
+CREATE TABLE mypokemon (
+	number INT, 
+    name VARCHAR(20),
+    type VARCHAR(20),
+    height FLOAT,
+    weight FLOAT,
+    attack FLOAT,
+    defense FLOAT,
+    speed FLOAT
+    ) ; 
+    
+INSERT INTO mypokemon (number, name, type, height, weight, attack, defense, speed)
+VALUE (10, 'caterpie', 'bug', 0.3,2.9,30,35,45),
+	  (25, 'pikachu', 'electric', 0.4,6,55,40,90),
+      (26, 'raichu', 'electric', 0.8,30,90,55,110),
+      (133, 'eevee', 'normal', 0.3,6.5,55,50,55),
+      (152, 'chikorita', 'grass', 0.9,6.4,49,65,45) ; 
+# MISSION 1. 123 x 456 
+SELECT 123*456; 
+
+# MISSION 2. 2310 / 30
+SELECT 2310 / 30; 
+
+# MISSION 3. '피카츄'라는 문자열을 '포켓몬'이라는 이름의 별명을 가진 컬럼으로 가져오기
+SELECT '피카츄' AS '포켓몬' ; 
+
+# MISSION 4. 포켓몬 테이블에서 모든 포켓몬들의 컬럼과 값 전체를 가져오기 
+SELECT * FROM mypokemon ; 
+
+# MISSION 5. 포켓몬 테이블에서 모든 포켓몬들의 이름 가져오기 
+SELECT name FROM mypokemon ; 
+
+# MISSION 6. 포켓몬 테이블에서 모든 포켓몬들의 이름과 키, 몸무게 가져오기 
+SELECT name,height,weight FROM mypokemon ; 
+
+# MISSION 7. 포켓몬 테이블에서 포켓몬들의 키를 중복 제거하고 가져오기 
+SELECT DISTINCT height FROM mypokemon ;
+
+# MISSION 8. 포켓몬 테이블에서 모든 포켓몬들의 공격력을 2배 해서 'attack2'라는 별명으로 이름과 함께 가져오기 
+SELECT name, attack*2 AS attack2 
 FROM mypokemon ; 
 
-# MISSION 1. 포켓몬 테이블에서 포켓몬들의 이름과 키, 몸무게 가져오기
-SELECT name, height, weight
-FROM mypokemon ;
+# MISSION 9. 포켓몬 테이블에서 모든 포켓몬들의 이름을 '이름'이라는 한글 별명으로 가져오기
+SELECT name AS 이름
+FROM mypokemon ; 
 
-# MISSION 2. 포켓몬 테이블에서 포켓몬들의 이름과 공격력, 방어력, 속도 가져오기 
-SELECT name, attack, defense, speed
-FROM mypokemon ;
+# MISSION 10. 포켓몬 테이블에서 모든 포켓몬들의 공격력은 '공격력' 이라는 한글 별명으로, 방어력은 '방어력'이라는 한글 별명으로 가져오기 
+SELECT attack as 공격력, defense AS 방어력 
+FROM mypokemon; 
+
+# MISSION 11. 포켓몬 테이블에서 모든 포켓몬들의 키를 cm단위로 환산하여 'height(cm)'라는 별명으로 가져오기 
+SELECT height * 100 AS 'height(cm)' 
+FROM mypokemon;
+
+# MISSION 12. 포켓몬 테이블에서 첫 번째 로우에 위치한 포켓몬 데이터만 컬럼 전체 가져오기 
+SELECT * 
+FROM mypokemon 
+LIMIT 1;
+
+# MISSION 13. 포켓몬 테이블에서 2개의 포켓몬 데이터만 이름은 '영문명'이라는 별명으로, 키는 '키(m)'라는 별명으로, 몸무게는 '몸무게(kg)'이라는 별명으로 가져오기 
+SELECT name AS 영문명, height AS '키(m)', weight AS '몸무게(kg)'
+FROM mypokemon
+LIMIT 2; 
 
 /*
-# MISSION 3. 포켓몬 테이블에서 포켓몬들의 이름과 능력치의 합 가져오기 
+# MISSION 14. 포켓몬 테이블에서 포켓몬들의 이름과 능력치의 합 가져오기
 	- Condition1. 능력치의 합은 공격력, 방어력, 속도의 합을 의미
     - Condition2. 능력치의 합은 'total'이라는 별명으로 가져오기
     - HINT. 숫자형 데이터 타입 컬럼 간에는 연산이 가능 
 */
-SELECT name, attack+ defense+speed AS TOTAL
-FROM mypokemon;
+SELECT name, attack+defense+speed AS total 
+FROM mypokemon ; 
+
+/*
+# MISSION 15. 포켓몬들의 이름과, BMI지수 가져오기 BMI지수는 'BMI'라는 별명으로 가져오기 
+	- BMI 지수 = 몸무게(kg) / 키(m)^2
+*/
+SELECT name, weight/height^2 as BMI 
+FROM mypokemon ; 
+
